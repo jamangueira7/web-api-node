97c27615dbf679eb3d0b19634819a7c93ac9e5ea
João Mangueira <jamjs7@gmail.com>
2021-03-21 22:13:55 -0300

    API pronta

--- src/index.js
+++ src/index.js
@@ -14,21 +14,28 @@ const routes = {
     },
     '/heroes:post': async (request, response) => {
         for await (const data of request) {
-            const item = JSON.parse(data);
-            const hero = new Hero(item);
-            const { error, valid } = hero.isValid();
-            if(!valid) {
-                response.writeHead(400, DEFAULT_HEADER);
-                response.write(JSON.stringify({ error: error.join(',') }));
-                // só jogamos o retorno pois sabemos que é um objeto body por requisicao
-                // se fosse um arquivo, ele poderia chamar mais de uma vez, aí removeriamos o return
+            try {
+                //Simular erro
+                //await Promise.reject('/heroes:post');
+                const item = JSON.parse(data);
+                const hero = new Hero(item);
+                const { error, valid } = hero.isValid();
+                if(!valid) {
+                    response.writeHead(400, DEFAULT_HEADER);
+                    response.write(JSON.stringify({ error: error.join(',') }));
+                    // só jogamos o retorno pois sabemos que é um objeto body por requisicao
+                    // se fosse um arquivo, ele poderia chamar mais de uma vez, aí removeriamos o return
+                    return response.end();
+                }
+
+                const id = await heroService.create(hero);
+                response.writeHead(201, DEFAULT_HEADER);
+                response.write(JSON.stringify({ success: 'User created with success!', id }));
                 return response.end();
+            } catch (err) {
+                return handleError(response)(err);
             }
 
-            const id = await heroService.create(hero);
-            response.writeHead(201, DEFAULT_HEADER);
-            response.write(JSON.stringify({ success: 'User created with success!', id }));
-            return response.end();
         }
     },
     default:  (request, response) => {
@@ -39,8 +46,10 @@ const routes = {
 
 const handleError = (response) => {
     return error => {
+        console.error('Internal Error***', error)
         response.writeHead(500, DEFAULT_HEADER);
         response.write(JSON.stringify({ error: 'Internal Server Error!!!' }));
+
         response.end();
     };
 }
@@ -53,9 +62,9 @@ const handler = (request, response) => {
 
     const key = `/${route}:${method.toLowerCase()}`;
 
-    response.writeHead(200, DEFAULT_HEADER);
-
     const chosen = routes[key] || routes.default;
+
+    response.writeHead(200, DEFAULT_HEADER);
 
     return chosen(request, response).catch(handleError(response));
 }
